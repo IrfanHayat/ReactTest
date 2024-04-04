9a92637ae6e439e7a6e9e2f5e4e2f81db3eebafc
IrfanHayat <irfanhayat55@gmail.com>
2024-04-04 01:14:20 -0700

    simple server implementation

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-"hello server"
+const express = require('express');
+const app = express();
+const port = 3000; // or any port you prefer
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
