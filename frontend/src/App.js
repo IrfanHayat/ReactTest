7601944288b17891f9a7e61675239a253492b2cf
IrfanHayat <irfanhayat55@gmail.com>
2024-04-05 03:53:59 -0700

    simple structure of frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import HomeScreen from './screens/HomeScreen'
+
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      
+      <main className='container content'>
+        <Routes>
+          <Route path='/' element={<HomeScreen />} />
+          <Route path='/login' element={<LoginScreen />} />
+          <Route path='/register' element={<RegisterScreen />} />
+
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </main>
+    </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,8 @@
+import { useSelector } from 'react-redux'
+import '../styles/profile.css'
+
+const ProfileScreen = () => {
+  
+}
+
+export default ProfileScreen
