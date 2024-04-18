6754cd4163e8801f302027cf18e49c97fba24bcb
IrfanHayat <irfanhayat55@gmail.com>
2024-04-18 02:15:44 -0700

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,8 @@ function App() {
       
       <main className='container content'>
         <Routes>
-          <Route path='/' element={<HomeScreen />} />
+          <Route path='/' element={<LoginScreen />} />
+          <Route path='/home' element={<HomeScreen />} />
           <Route path='/login' element={<LoginScreen />} />
           <Route path='/register' element={<RegisterScreen />} />
         
